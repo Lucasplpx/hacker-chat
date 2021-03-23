8475dd205fcf2d46d01e387c3a42333860e715d3
Lucas Passos <lucasplpx@gmail.com>
2021-03-23 19:23:07 -0300

    Sockets HTTP e Arquitetura Orientada a Eventos

--- client/index.js
+++ client/index.js
@@ -1,7 +1,15 @@
 import Events from 'events';
+import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 import TerminalController from './src/terminalController.js';
 
-const componentEmitter = new Events();
-const controller = new TerminalController();
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
 
-await controller.initializeTable(componentEmitter);
+const componentEmitter = new Events();
+
+const socketClient = new SocketClient(config);
+socketClient.initialize();
+
+// const controller = new TerminalController();
+// await controller.initializeTable(componentEmitter);
